99bb311eac2e0b8294f347cee61ea2823fb3f487
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-06-12 20:14:47 +0200

    json

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.route.js'
 import postRouter from './routes/post.route.js'
@@ -7,6 +8,7 @@ import dotenv from 'dotenv';
 const app = express()
 dotenv.config();
 
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/posts', postRouter)
